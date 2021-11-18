3d2456c4a274a426977b0d02f8c6594152b0098e
Arthur Plautz Ventura <atr.plautz@gmail.com>
2021-11-18 19:46:53 -0300

    Context Stats Analysis

--- experiments/specialist/specialist_data.py
+++ experiments/specialist/specialist_data.py
@@ -1,6 +1,7 @@
 # Configuration for running Jupyter Notebooks with project models
 import sys
 import os
+import pandas as pd
 
 module_path = os.path.abspath(os.path.join(os.pardir, os.pardir))
 if module_path not in sys.path:
@@ -8,11 +9,8 @@ if module_path not in sys.path:
 
 # Transforming data
 from models.data import DataManager
-from models.stats import Stats
 
 manager = DataManager('../../configs/xdpole.yml')
 manager.extract()
 transformed = manager.transform()
 transformed.create_level_label()
-
-stats = Stats('../../data/xdpole', 10)
