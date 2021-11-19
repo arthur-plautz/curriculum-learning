e67e928dcbdf2e38c70633ded9708e79cd78fede
Arthur Plautz Ventura <atr.plautz@gmail.com>
2021-11-19 18:19:52 -0300

    Seed Conditions EDA

--- experiments/specialist/specialist_data.py
+++ experiments/specialist/specialist_data.py
@@ -9,8 +9,12 @@ if module_path not in sys.path:
 
 # Transforming data
 from models.data import DataManager
+from sklearn.model_selection import train_test_split
 
 manager = DataManager('../../configs/xdpole.yml')
 manager.extract()
 transformed = manager.transform()
 transformed.create_level_label()
+
+# split train/test data
+X_train, X_test, y_train, y_test = train_test_split(transformed.X, transformed.level, test_size=0.33, random_state=42)
