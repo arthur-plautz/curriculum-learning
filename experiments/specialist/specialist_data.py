49a102c5422ade8f8dc2dfa8085922652ef8430a
Arthur Plautz Ventura <atr.plautz@gmail.com>
2021-11-23 00:58:24 -0300

    New Models Tested

--- experiments/specialist/specialist_data.py
+++ experiments/specialist/specialist_data.py
@@ -16,12 +16,22 @@ manager.extract()
 transformed = manager.transform()
 
 def level(value):
-    if value > 100:
+    if value > 150:
         return 'good'
     else:
         return 'bad'
 
 transformed.create_level_label(level_func=level)
 
-# split train/test data
-X_train, X_test, y_train, y_test = train_test_split(transformed.X, transformed.level, test_size=0.33, random_state=42)
+def get_stage(stage):
+    stages = {
+        'total': transformed.data,
+        'birth': transformed.data.query('index < 7500'),
+        'growth': transformed.data.query('index > 7500 and index < 12000'),
+        'child': transformed.data.query('index < 10000'),
+        'first_mature': transformed.data.query('index > 12000 and index < 30000'),
+        'last_mature': transformed.data.query('index > 30000'),
+        'mature': transformed.data.query('index > 9000'),
+    }
+    transformed.data = stages.get(stage)
+    return train_test_split(transformed.X, transformed.level, test_size=0.33, random_state=42)
